7f5e5d314608f6615c8c605df72b2df8a3398657
fawzywassel <fawzy@MacBooks.local>
2017-03-23 14:47:46 +0200

    http-service

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import {MattDamonPipe} from '../../pipes/matt-damon'
-
 import { NavController } from 'ionic-angular';
-
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
   title ="Parallax Header";
-  constructor(public navCtrl: NavController) {
-
+  posts: any;
+  constructor(public navCtrl: NavController ,public http: Http) {
+    this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=10').map(res => res.json()).subscribe(data => {
+     this.posts = data.data.children;
+     console.log(this.posts)
+ });
   }
 
 }
